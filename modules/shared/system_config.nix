70f8529c206e953bdf0a83a133fee2ae545e18c1
Timur <tshamsrakhmanov@gmail.com>
2026-07-14 17:05:11 +0400

    move aliases back to general

--- modules/shared/system_config.nix
+++ modules/shared/system_config.nix
@@ -2,6 +2,26 @@
 
 {
 
+  # ---------
+  # -- VIM --
+  # ---------
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  # --------------------
+  # -- SHELL ALIASES  --
+  # --------------------
+  environment.shellAliases = {
+    l = "eza -l --color=always --group-directories-first --icons";
+    ll = "eza -al --color=always --group-directories-first --icons";
+    zz = "exit";
+    qq = "ps -eo pid,command | grep";
+    update = "sudo nixos-rebuild swtich --flake .";
+    ".." = "cd .. && l";
+  };
+
   # ------------------
   # -- BOOT LOADER  --
   # ------------------

ed909c1e59b9fca43ebedf1a079f898e5f0dca7f
Timur <tshamsrakhmanov@gmail.com>
2026-07-15 11:14:04 +0400

    move aliases from system config to home

--- hosts/laptop-old/home.nix
+++ hosts/laptop-old/home.nix
@@ -1,25 +1,39 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "timur";
-  home.homeDirectory = "/home/timur";
+    home.username = "timur";
+    home.homeDirectory = "/home/timur";
 
-  home.packages = [];
+    home.packages = [];
 
 
-  programs.git = {
-    enable = true;
-    userName = "Timur Shamsrakhmanov";
-    userEmail = "tshamsrakhmanov@gmail.com";
+    programs.git = {
+        enable = true;
+        userName = "Timur Shamsrakhmanov";
+        userEmail = "tshamsrakhmanov@gmail.com";
 
-    aliases = {
-      st = "status -sb";
-      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      cm = "commit -m";
+        aliases = {
+            st = "status -sb";
+            lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+            cm = "commit -m";
+        };
+
     };
 
-  };
 
-  # Minimal configuration to test
-  home.stateVersion = "26.05";
+    programs.bash = {
+        enable = true;
+        enableCompletion = true;
+        shellAliases = {
+            l = "eza -l --color=always --group-directories-first --icons";
+            ll = "eza -al --color=always --group-directories-first --icons";
+            zz = "exit";
+            qq = "ps -eo pid,command | grep";
+            update = "sudo nixos-rebuild swtich --flake .";
+            ".." = "cd .. && l";
+        };
+    };
+
+# Minimal configuration to test
+    home.stateVersion = "26.05";
 }
--- modules/shared/system_config.nix
+++ modules/shared/system_config.nix
@@ -13,14 +13,14 @@
   # --------------------
   # -- SHELL ALIASES  --
   # --------------------
-  environment.shellAliases = {
-    l = "eza -l --color=always --group-directories-first --icons";
-    ll = "eza -al --color=always --group-directories-first --icons";
-    zz = "exit";
-    qq = "ps -eo pid,command | grep";
-    update = "sudo nixos-rebuild swtich --flake .";
-    ".." = "cd .. && l";
-  };
+  #environment.shellAliases = {
+  #  l = "eza -l --color=always --group-directories-first --icons";
+  #  ll = "eza -al --color=always --group-directories-first --icons";
+  #  zz = "exit";
+  #  qq = "ps -eo pid,command | grep";
+  #  update = "sudo nixos-rebuild swtich --flake .";
+  #  ".." = "cd .. && l";
+  #};
 
   # ------------------
   # -- BOOT LOADER  --

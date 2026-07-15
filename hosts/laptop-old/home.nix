6b1358ea52443757bf4a31cb05fc71aaf1eee03a
Timur <tshamsrakhmanov@gmail.com>
2026-07-15 13:16:33 +0400

    typo in switch

--- hosts/laptop-old/home.nix
+++ hosts/laptop-old/home.nix
@@ -33,7 +33,7 @@
             ll = "eza -al --color=always --group-directories-first --icons";
             zz = "exit";
             qq = "ps -eo pid,command | grep";
-            update = "sudo nixos-rebuild swtich --flake .";
+            update = "sudo nixos-rebuild switch --flake .";
             ".." = "cd .. && l";
         };
     };

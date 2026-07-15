fc599ee8da73524a3b1b528547215b73d0991b15
Timur <tshamsrakhmanov@gmail.com>
2026-07-15 11:35:29 +0400

    another

--- flake.nix
+++ flake.nix
@@ -43,13 +43,16 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               #home-manager.users.timur = import ./hosts/laptop-old/home.nix;
-              home-manager.users.timur = { ... } : {
-                imports = [ ./hosts/laptop-old/home.nix ];
-                home.file = {
-                  ".tmux.conf".source = "${tmux-config}/.tmux.conf";
-                  ".vimrc".source = "${vim-config}/.vimrc";
-                };
+              home-manager.users.timur = import ./hosts/laptop-old/home.nix {
+                inherit tmux-config vim-config;
               };
+              #home-manager.users.timur = { ... } : {
+              #  imports = [ ./hosts/laptop-old/home.nix ];
+              #  home.file = {
+              #    ".tmux.conf".source = "${tmux-config}/.tmux.conf";
+              #    ".vimrc".source = "${vim-config}/.vimrc";
+              #  };
+              #};
               # Or if you want to use the shared modules:
               # home-manager.users.your-username = { ...homeModules, ...import ./hosts/laptop-old/home.nix };
             }

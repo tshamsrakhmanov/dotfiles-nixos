2a1a148a8b77c962d4571c54229f58439ef3ffe4
Timur <tshamsrakhmanov@gmail.com>
2026-07-15 11:09:16 +0400

    add git to hm

--- hosts/laptop-old/home.nix
+++ hosts/laptop-old/home.nix
@@ -6,6 +6,20 @@
 
   home.packages = [];
 
+
+  programs.git = {
+    enable = true;
+    userName = "Timur Shamsrakhmanov";
+    userEmail = "tshamsrakhmanov@gmail.com";
+
+    aliases = {
+      st = "status -sb";
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      cm = "commit -m";
+    };
+
+  };
+
   # Minimal configuration to test
   home.stateVersion = "26.05";
 }
